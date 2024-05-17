f60ac43e2fc6c66eb055f56290fb835c086c8cc0
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-05-17 12:56:29 +0300

    add caching to scraper

--- scrape.mjs
+++ scrape.mjs
@@ -1,11 +1,24 @@
 import * as cheerio from 'cheerio';
+import fs from 'node:fs';
 const delay = (delayInms) => {
     return new Promise(resolve => setTimeout(resolve, delayInms));
 };
 
+if(!fs.existsSync('./cache')){
+    fs.mkdirSync('./cache');
+}
+
 for(let i = 2927; i>2917; i--){
-    let response = await fetch(`https://xkcd.com/${i}/`);
-    response = await response.text();
+    let response;
+    if(!fs.existsSync(`./cache/${i}.html`)){
+        await delay(3000);
+        response = await fetch(`https://xkcd.com/${i}/`);
+        response = await response.text();
+        fs.writeFileSync(`./cache/${i}.html`, response);
+        console.log('LIVE REQUEST');
+    } else {
+        response = fs.readFileSync(`./cache/${i}.html`);
+    }
     //console.log(response);
     
     const $ = cheerio.load(response);
@@ -13,5 +26,6 @@ for(let i = 2927; i>2917; i--){
     console.log(img.attr('src'));
     console.log(img.attr('alt'));
     console.log(img.attr('title'));
-    await delay(3000);
+    console.log(img.attr('srcset'));
+    
 }
